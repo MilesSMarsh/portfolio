d1992b50d5457482da1b39c38b271b79720b1422
Miles Marsh <msmarsh@ucsc.edu>
2023-12-07 11:04:35 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+let slideIndex = [1,1,1,1,1];
+/* Class the members of each slideshow group with different CSS classes */
+let slideId = ["runeomancy", "rearwindow", "rewindplatformer","guarden","runner"];
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let slides = document.getElementsByClassName(slideId[no]);
+  if (n > slides.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex[no]-1].style.display = "block";
+}
+
+var coll = document.getElementsByClassName("collapsible");
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
